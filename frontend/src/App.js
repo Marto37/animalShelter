b785444635890e9031a08eeb763c0262434b7499
Saurav Ghosal <sghosal9@gatech.edu>
2019-11-21 19:09:14 -0500

    added image to card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,11 @@ import PetCard from "./components/PetCard";
 function App() {
   return (
     <div className="App">
-      <PetCard name="hello" body="body" />
+      <PetCard
+        name="hello"
+        body="body"
+        img="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+      />
     </div>
   );
 }
--- frontend/src/components/PetCard.js
+++ frontend/src/components/PetCard.js
@@ -4,7 +4,7 @@ import Button from "react-bootstrap/Button";
 
 const PetCard = props => (
   <Card style={{ width: "18rem" }}>
-    <Card.Img variant="top" src="holder.js/100px180" />
+    <Card.Img variant="top" src={props.img} />
     <Card.Body>
       <Card.Title>{props.name}</Card.Title>
       <Card.Text>{props.body}</Card.Text>
