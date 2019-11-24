cd4f0d41677a71b2445c750bdc6fae67f6171fa1
Saurav Ghosal <sghosal9@gatech.edu>
2019-11-24 14:15:01 -0500

    request layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,25 +7,39 @@ import Navigation from "./components/Navigation";
 const App = () => {
   const [pets, setPets] = React.useState([]);
 
-  fetch("getpets")
-    .then(response => {
-      return response.json();
+  // creates cards on startup
+  React.useEffect(() => {
+    getPets();
+  }, []);
+
+  const getPets = () => {
+    fetch("/pets")
+      .then(res => res.json())
+      .then(data => setPosts(data.pets));
+  };
+
+  const addPet = postData => {
+    fetch("/pets", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers
     })
-    .then(pet => {
-      setPets([...pets, pet]);
-    })
-    .catch(error => {
-      console.log(error);
-    });
+      .then(response => response.json())
+      .then(data => setPets([...pets, data.pet]));
+  };
 
   return (
     <div className="App">
       <Navigation />
-      <PetCard
-        name="hello"
-        body="body"
-        img="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
-      />
+      <Row>
+        {pets.map(pet => (
+          <PetCard
+            name="hello"
+            body="body"
+            img="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+          />
+        ))}
+      </Row>
     </div>
   );
 };
